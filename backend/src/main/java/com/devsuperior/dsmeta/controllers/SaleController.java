713f7a983dcdad1e9e1b392fc44163825b5e1676
nicolasarruda <nlarruda@hotmail.com>
2022-09-07 23:01:27 -0300

    Update @Controller to @RestController

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -8,12 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
 import com.devsuperior.dsmeta.services.SmsService;
 
-@Controller
+@RestController
 @RequestMapping(value = "/sales")
 public class SaleController {
 
--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -18,16 +18,11 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	private static final Long MINUS_TIME_YEAR = 1L;
-	private static final Long MINUS_TIME_MONTH = 12L;
-	private static final Long MINUS_TIME_WEEK = 48L;
-	private static final Long MINUS_TIME_DAY = 365L;
-	
 	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
 		
 		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
 		
-		LocalDate min = minDate.equals("") ? today.minusDays(MINUS_TIME_DAY) : LocalDate.parse(minDate);
+		LocalDate min = minDate.equals("") ? today.minusDays(365L) : LocalDate.parse(minDate);
 		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
 		
 		Page<Sale> sales = repository.findSales(min, max, pageable);
